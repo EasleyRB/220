622e495b3cfbb5b0618a62972fe9dd7f1806ded1
Blake <easleyrb@g.cofc.edu>
2022-04-01 23:50:44 -0400

    HW9

--- assignments/hw9/hw9.py
+++ assignments/hw9/hw9.py
@@ -1,25 +1,47 @@
+from random import randint
+
+
 def get_words(file_name):
-    pass
+    file_name = open(file_name, 'r')
+    words = file_name.readlines()
+    return words
 
 
 def get_random_word(words):
-    pass
+    random_word = randint(0, len(words) - 1)
+    secret_word = words[random_word].strip('\n')
+    return secret_word
 
 
 def letter_in_secret_word(letter, secret_word):
-    pass
+    if letter in secret_word:
+        return True
+    return False
 
 
 def already_guessed(letter, guesses):
-    pass
+    if letter in guesses:
+        return True
+    return False
 
 
 def make_hidden_secret(secret_word, guesses):
-    pass
+    word = ''
+    for letter in secret_word:
+        if letter in guesses:
+            word = word + letter + ' '
+        else:
+            word = word + '_ '
+    guessed_word = word.strip()
+    return guessed_word
 
 
 def won(guessed):
-    pass
+    guessed = guessed.split(' ')
+    not_guessed = guessed.count('_')
+    if not_guessed == 0:
+        return True
+    return False
 
 
 def play_graphics(secret_word):
@@ -27,7 +49,8 @@ def play_graphics(secret_word):
 
 
 def play_command_line(secret_word):
-    pass
+    words = get_words("words.txt")
+    secret_word = get_random_word(words)
 
 
 if __name__ == '__main__':
